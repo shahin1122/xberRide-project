f7022463420d7fdddd64c467e9f8055cf0547987
shahin1122 <passportscotch7@gmail.com>
2021-03-20 23:34:30 +0600

    resolved

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -3,6 +3,22 @@ import { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import location from '../../data/location.json'
 import css from './Destination.css';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+
+// code for map
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
+
+
 
 
 
@@ -13,14 +29,16 @@ const Destination = () => {
 
     //Use params doesn't work!!
 
-    const {carId} = useParams();
-    const [car , setCar] = useState();
+    const { carId } = useParams();
+    const [car, setCar] = useState();
 
-     
+    const [show , setShow] = useState(false)
 
 
 
-    
+
+
+
 
     console.log(carId);
 
@@ -28,75 +46,99 @@ const Destination = () => {
     const product = location.find(pd => pd.key === carId)
     console.log(product);
 
-    const clickHandeler=()=>{
-        
-        var isClicked = false ;
+    const clickHandeler = () => {
+
+        var isClicked = false;
 
     }
 
-    
-
 
     
 
-    
+
+
+
+
+
+
     return (
         <div>
 
-            
+
 
 
             <div className='searchArea'>
                 <div>
-                <h4>Pick From</h4>
-                <input value="" type="text"/>
-                <h4>Pick to</h4>
-                <input value="" type="text"/>
-                <br/>
-                <button onClick={clickHandeler} >Search</button>
-                </div>
-            </div>
-
-
-
-            
-
-            
-           
-            <div className='resultArea'>
-
+                    <h4>Pick From</h4>
+                    <input name ="from" type="text" />
+                    <h4>Pick to</h4>
+                    <input name="to" type="text" />
+                    <br />
                     
-            <div className="carCard">
-                <p>{product.name}</p>
-                <img src={product.img} alt=""/>
-                <span>{product.rent}</span>
-
-            </div>
-
-            <div className="carCard">
-                <p>{product.name}</p>
-                <img src={product.img} alt=""/>
-                <span>{product.rent}</span>
-
-            </div>
-
-            <div className="carCard">
-                <p>{product.name}</p>
-                <img src={product.img} alt=""/>
-                <span>{product.rent}</span>
-
-            </div>  
-                        
-                            
-   
-               
+                </div>
                 
+                <button onClick={()=>setShow(true)} >Search</button>
 
             </div>
+
+
+
+
+
+
+
+           {
+               show ? 
+               
+               <div className='resultArea'>
+
+
+               <div className="carCard">
+                   <p>{product.name}</p>
+                   <img src={product.img} alt="" />
+                   <span>{product.rent}</span>
+
+               </div>
+
+               <div className="carCard">
+                   <p>{product.name}</p>
+                   <img src={product.img} alt="" />
+                   <span>{product.rent}</span>
+
+               </div>
+
+               <div className="carCard">
+                   <p>{product.name}</p>
+                   <img src={product.img} alt="" />
+                   <span>{product.rent}</span>
+
+               </div>
+
+
+
+           </div> : null
+
+
+
+           }
+
+
+            <LoadScript
+                googleMapsApiKey="https://maps.googleapis.com/maps/api/js?key=&callback=initMap"
+            >
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={10}
+                >
+                    { /* Child components, such as markers, info windows, etc. */}
+                    <></>
+                </GoogleMap>
+            </LoadScript>
 
         </div>
 
-           
+
     );
 };
 
